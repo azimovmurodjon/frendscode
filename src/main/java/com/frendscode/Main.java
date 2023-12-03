c20eba4b2591a0d0004a1f060e306dadae9a8ce8
M.J. Azimov <96605136+azimovmurodjon@users.noreply.github.com>
2023-12-03 00:19:03 -0500

    Adding Beans to Main class

--- src/main/java/com/frendscode/Main.java
+++ src/main/java/com/frendscode/Main.java
@@ -2,6 +2,7 @@ package com.frendscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -11,14 +12,20 @@ public class Main {
     //Our fake DB
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(Main.class, args);
+
+        String[] beanDefinitionNames =
+                applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
     }
 
     //    @RequestMapping(
 //            path = "api/v1/customers",
 //            method = RequestMethod.GET
 //    )
-
 
 
 }
