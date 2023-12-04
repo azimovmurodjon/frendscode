d36f16496f9dbc6961714a17efa0d44e15e9e415
mac_book_pro <azimovm.j@gamil.com>
2023-12-04 15:43:40 -0500

    Added Customer Repo, Customer JPA Data Access Service Customer Data list updated, main page is updated etc

---
+++ src/main/java/com/frendscode/Customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.frendscode.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+
+}
